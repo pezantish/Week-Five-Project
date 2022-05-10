21471ca0ce749cce43538909a02c668056dc0393
pezantish <e.e.a.sadek@gmail.com>
2022-05-10 16:49:23 +0100

    minor updates

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -25,21 +25,17 @@ public class Item {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
 	
-
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 
 	public Double getPrice() {
 		return price;
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,10 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) DEFAULT NULL,
+    `price` DOUBLE DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
