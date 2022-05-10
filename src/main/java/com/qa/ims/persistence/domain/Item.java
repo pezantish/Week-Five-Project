255a25452c09045e7a39f31cc2f79ecfdc6b7bc5
pezantish <e.e.a.sadek@gmail.com>
2022-05-10 16:38:47 +0100

    INITIAL ITEM FUNCTIONALITY

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,94 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+
+	private Long id;
+	private String name;
+	private Double price;
+
+
+	public Item(String name, Double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+	
+	
+	public Item(Long id, String name, Double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "id:" + id + " description:" + name + " price:" + price;
+	}
+
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+//		result = prime * result + ((id == null) ? 0 : id.hashCode());
+//		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+//		return result;
+//	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (getName() == null) {
+			if (other.getName() != null)
+				return false;
+		} else if (!getName().equals(other.getName()))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (price != other.price)
+			return false;
+		return true;
+	}
+
+}
